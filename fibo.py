4dc3922dfcc783c426b0c2669c51c06861fabfa5
Liberti Thomas <libertitom@gmail.com>
2022-07-19 15:07:10 +0200

    test fibo2

--- fibo.py
+++ fibo.py
@@ -2,16 +2,17 @@
 doc
 """
 nterms = int(input("Entrez un nombre: "))
- 
+
 n1 = 0
 n2 = 1
- 
+
 print("\n la suite fibonacci est :")
 print(n1, ",", n2, end=", ")
- 
+
 for i in range(2, nterms):
-  suivant = n1 + n2
-  print(suivant, end=", ")
- 
-  n1 = n2
-  n2 = suivant
+    suivant = n1 + n2
+    print(suivant, end=", ")
+    
+    n1 = n2
+    n2 = suivant
+    
